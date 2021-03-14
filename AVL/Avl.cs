e603626f40600ccac19f8ceb681b1146e91c5b1f
Mihir Takalkar <Mihir.Takalkar@GMR.local>
2021-03-14 12:47:17 -0700

    Finished Rotate and Balance Functions.

---
+++ AVL/Avl.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVL
+{
+    class Avl<T> where T : IComparable<T>
+    {
+        Node<T> root;
+        int nodeCount;
+
+        //insert
+        Node<T> Insert(Node<T> currentNode, T value)
+        {
+
+        }
+
+
+        //inserthelper
+        void InsertHelper()
+        {
+
+        }
+
+        //rotations
+        Node<T> RightRotate(Node<T> unbalancedNode)
+        {
+            Node<T> pivot = unbalancedNode.LeftNode;
+            unbalancedNode.RightNode = pivot.RightNode;
+            pivot.RightNode = unbalancedNode;
+            return pivot;
+        }
+
+        Node<T> LeftRotate(Node<T> unbalancedNode)
+        {
+            Node<T> pivot = unbalancedNode.RightNode;
+            unbalancedNode.LeftNode = pivot.LeftNode;
+            pivot.LeftNode = unbalancedNode;
+            return pivot;
+        }
+
+        //balance
+        Node<T> Balance(Node<T> currentNode)
+        {
+            //if tree is leaning left but subtree is leaning right
+            if(currentNode.Balance < -1)
+            {
+                if(currentNode.LeftNode.Balance > 0)
+                {
+                    currentNode.LeftNode = LeftRotate(currentNode.LeftNode);
+                }
+                currentNode = RightRotate(currentNode);
+            }
+
+            //if tree is leaning right but subtree is leaning left
+            else if(currentNode.Balance > 1)
+            {
+                if(currentNode.RightNode.Balance < 0)
+                {
+                    currentNode.RightNode = RightRotate(currentNode.RightNode);
+                }
+                currentNode = LeftRotate(currentNode);
+            }
+
+            return currentNode;
+        }
+
+        //remove
+
+    }
+}
---
+++ AVL/Node.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVL
+{
+      
+    class Node<T> where T : IComparable<T> 
+    {
+        public T Val { get; set; }
+        public Node<T> LeftNode { get; set; }
+        public Node<T> RightNode { get; set; }
+
+        public int ChildCount
+        {
+            get
+            {
+                int count = 0;
+                if(LeftNode != null)
+                {
+                    count++;
+                }
+                if(RightNode != null)
+                {
+                    count++;
+                }
+                return count;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                int leftHeight = 0;
+                if(LeftNode != null)
+                {
+                    leftHeight = LeftNode.Height;
+                }
+
+                int rightHeight = 0;
+                if(RightNode != null)
+                {
+                    rightHeight = RightNode.Height;
+                }
+
+                return Math.Max(leftHeight, rightHeight) + 1;
+            }
+        }
+
+        public int Balance
+        {
+            get
+            {
+                int leftHeight = 0;
+                if (LeftNode != null)
+                {
+                    leftHeight = LeftNode.Height;
+                }
+
+                int rightHeight = 0;
+                if (RightNode != null)
+                {
+                    rightHeight = RightNode.Height;
+                }
+
+                return rightHeight - leftHeight;
+            }
+        }
+
+        public Node(T val, Node<T> rightChild = null, Node<T> leftChild = null)
+        {
+            this.Val = val;
+            LeftNode = leftChild;
+            RightNode = rightChild;
+        }
+    }
+}
